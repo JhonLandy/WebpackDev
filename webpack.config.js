005af8b340f1fd9a7e5b56bc180047ca4d076d70
wb.yuanchenglang <wb.yuanchenglang@mesg.corp.netease.com>
2020-10-20 17:58:27 +0800

    splitChunks

--- webpack.config.js
+++ webpack.config.js
@@ -5,120 +5,165 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    entry: './src/main.js',//单入口
+    "mode": 'development',
+    "devtool": 'inline-source-map',
+    "entry": './src/main.js',//单入口
     // entry: {
     //     main1:"./src/main1",
     //     main2:"./src/main2"
     // },//多入口
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "./js/[name]_[hash].js", //使用[name]打包出来的js文件会分别按照入口文件配置的属性来命名
+    "output": {
+        "path": path.resolve(__dirname, "dist"),
+        "filename": "./js/[name]_[hash].js", //使用[name]打包出来的js文件会分别按照入口文件配置的属性来命名
     },
-    resolve: {
-        extensions: ['.js', '.vue', '.json', 'css'],
-        alias: {
+    "resolve": {
+        "extensions": ['.js', '.vue', '.json', 'css'],
+        "alias": {
             'vue$': 'vue/dist/vue.esm.js',
             '@': path.resolve('src')
         }
     },
-    module: {
-        rules: [
+    "module": {
+        "rules": [
             {
-                test: /\.(vue)$/,
-                use: [
+                "test": /\.vue$/,
+                "use": [
                     {
-                        loader: 'vue-loader',
+                        "loader": 'vue-loader',
                     }
                 ],
             },
             {
-                test: /\.(css)$/,
-                use: [
+                "test": /\.css$/,
+                "use": [
                     {
-                        loader: MiniCssExtractPlugin.loader
+                        "loader": MiniCssExtractPlugin.loader
                     },
                     'css-loader'
                 ]
             },
             {
-                test: /\.(js)$/,
-                use: 'babel-loader',
-                exclude:/node_modules/
+                "test": /\.js$/,
+                "use": 'babel-loader',
+                "exclude":/node_modules/
             },
             {
-                test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
-                loader: [
+                "test": /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+                "loader": [
                     {
-                        loader: 'url-loader',
-                        options: {
-                            name: '[name].[ext]',
+                        "loader": 'url-loader',
+                        "options": {
+                            "name": '[name].[ext]',
                         }
                     }
                 ]
             },
             {
-                test: /\.(jpg)$/,
-                use: [
+                "test": /\.(jpg|png|jpeg|webp|gif)$/,
+                "loader": [
                     {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: './image',
-                            esModule: false,
+                        "loader": 'url-loader',
+                        "options": {
+                            "name": '[name].[ext]',
+                            "outputPath": './image',
+                            "esModule": false,
                         }
                     }
                 ]
             },
+            // {有 url-loader 无需使用，但url-loader 依赖file-loader，file-loader必须下载
+            //     "test": /\.(jpg)$/,
+            //     "use": [
+            //         {
+            //             "loader": 'file-loader',
+            //             "options": {
+            //                 "name": '[name].[ext]',
+            //                 "outputPath": './image',
+            //                 "esModule": false,
+            //             }
+            //         }
+            //     ]
+            // },
         ]
     },
-    plugins: [
+    "optimization": {
+        splitChunks: {
+            chunks: 'all',
+            minSize: 20000,
+            maxSize: 0,
+            minChunks: 1,
+            maxAsyncRequests: 30,
+            maxInitialRequests: 30,
+            automaticNameDelimiter: '~',
+            cacheGroups: {
+                elementUI: {
+                    name: 'chunk-elementUI', // split elementUI into a single package
+                    priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+                    test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+                },
+                styles: {
+                    name: 'styles',
+                    test: /\.css$/,
+                    chunks: 'all',
+                    enforce: true,
+                    priority: 20,
+                },
+                libs: {
+                    name: 'chunk-libs',
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: 10,
+                    chunks: 'initial' // only package third parties that are initially dependent
+                },
+            }
+        },
+        usedExports: true //Tree Shaking
+    },
+    "plugins": [
         new VueLoaderPlugin(),
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: "myWebPackDemo",
-            favicon: './public/favicon.ico',
-            template: './public/index.html',
-            filename: "index.html",
-            inject: true
+            "title": "myWebPackDemo",
+            "favicon": './public/favicon.ico',
+            "template": './public/index.html',
+            "filename": "index.html",
+            "inject": true
         }),
         new MiniCssExtractPlugin({
-            title: 'fuck',
-            filename: 'css/[name].[hash].css',
-            chunkFilename: 'css/[id]-[contenthash].css',
-            minify: { //压缩 html 文件
+            "title": 'fuck',
+            "filename": 'css/[name].[hash].css',
+            "chunkFilename": 'css/[id]-[contenthash].css',
+            "minify": { //压缩 html 文件
 
-                removeAttributeQuotes:true, //删除属性的双引号
+                "removeAttributeQuotes":true, //删除属性的双引号
 
-                collapseWhitespace:true //折叠空行，变成一行
+                "collapseWhitespace":true //折叠空行，变成一行
 
             },
         }),
     ],
-    devServer: {
+    "devServer": {
         // 生成的虚拟目录路径
         // contentBase: path.resolve(__dirname, "dist"),
         // 自动开启浏览器
-        openPage: 'page',
-        open: true,
+        "openPage": 'page',
+        "open": true,
         // 端口
-        port: 8081,
-        index: 'index.html',
-        historyApiFallback:true,
-        quiet: true,
-        proxy: {
+        "port": 8081,
+        "index": 'index.html',
+        "historyApiFallback":true,
+        "quiet": true,
+        "proxy": {
               '/ycl': {
-                  target: 'http://127.0.0.1:4000',
-                  changeOrigin: true,
-                  pathRewrite: {
+                  "target": 'http://127.0.0.1:4000',
+                  "changeOrigin": true,
+                  "pathRewrite": {
                       '^/ycl': ''
                   },
 
               }
         },
-        hot:true,
+        "hot":true,
         // 即使 HMR 不生效，也不去刷新整个页面(选择开启)
-        hotOnly:true
+        "hotOnly":true
     }
 }
